c3db2d9a3d71b0d27a5c28a1fc493cb4ac669f6e
Marta Derewicz <marta.derewicz05@gmail.com>
2026-05-07 12:57:51 +0200

    Add missing main menu files

---
+++ ViewModels/MainMenuViewModel.cs
@@ -0,0 +1,48 @@
+﻿using CardGamesApp.Services;
+using CommunityToolkit.Mvvm.Input;
+
+namespace CardGamesApp.ViewModels;
+
+public partial class MainMenuViewModel : ViewModelBase
+{
+	private readonly MainWindowViewModel _mainWindowViewModel;
+	private readonly PlayerService _playerService;
+
+	public int PlayerCount => _playerService.Players.Count;
+
+	public MainMenuViewModel(MainWindowViewModel mainWindowViewModel, PlayerService playerService)
+	{
+		_mainWindowViewModel = mainWindowViewModel;
+		_playerService = playerService;
+	}
+
+	[RelayCommand]
+	private void GoToPlayers()
+	{
+		_mainWindowViewModel.ShowPlayers();
+	}
+
+	[RelayCommand]
+	private void GoToWarGame()
+	{
+		_mainWindowViewModel.ShowWarGame();
+	}
+
+	[RelayCommand]
+	private void GoToTwentyOneGame()
+	{
+		_mainWindowViewModel.ShowTwentyOneGame();
+	}
+
+	[RelayCommand]
+	private void GoToHigherLowerGame()
+	{
+		_mainWindowViewModel.ShowHigherLowerGame();
+	}
+
+	[RelayCommand]
+	private void GoToHistory()
+	{
+		_mainWindowViewModel.ShowHistory();
+	}
+}
---
+++ Views/MainMenuView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace CardGamesApp.Views;
+
+public partial class MainMenuView : UserControl
+{
+	public MainMenuView()
+	{
+		InitializeComponent();
+	}
+}
