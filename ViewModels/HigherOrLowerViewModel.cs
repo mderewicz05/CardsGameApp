4791f677cadda49aef451ace465ae5942e396044
Julia Heleniak <julia.heleniak@student.put.poznan.pl>
2026-05-14 14:16:15 +0200

    dodano zgdaywanie symbolu karty po przegranej w higher or lower

--- ViewModels/HigherOrLowerViewModel.cs
+++ ViewModels/HigherOrLowerViewModel.cs
@@ -1,6 +1,4 @@
 using System;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
 using CardGamesApp.Models;
 using CardGamesApp.Services;
 using CommunityToolkit.Mvvm.Input;
@@ -11,13 +9,12 @@ namespace CardGamesApp.ViewModels
     {
         private readonly MainWindowViewModel _mainWindowViewModel;
         private readonly PlayerService _playerService;
-
         private Deck _deck;
         private Card _currentCard;
         private string _message = "";
         private int _score;
+        private bool _showSuitGuess; 
 
-        // W³aœciwoœci
         public Card CurrentCard
         {
             get => _currentCard;
@@ -35,11 +32,17 @@ namespace CardGamesApp.ViewModels
             get => _score;
             set { _score = value; OnPropertyChanged(); }
         }
+
+        public bool ShowSuitGuess
+        {
+            get => _showSuitGuess;
+            set { _showSuitGuess = value; OnPropertyChanged(); }
+        }
+
         public HigherOrLowerViewModel(MainWindowViewModel mainWindowViewModel, PlayerService playerService)
         {
             _mainWindowViewModel = mainWindowViewModel;
             _playerService = playerService;
-
             _deck = new Deck();
             _currentCard = _deck.DobierzKarte();
             _message = "Zgadnij: kolejna karta bedzie wyzsza czy nizsza?";
@@ -54,46 +57,55 @@ namespace CardGamesApp.ViewModels
         private void MakeGuess(bool guessedHigher)
         {
             Card nextCard = _deck.DobierzKarte();
-
-            if (nextCard == null)
-            {
-                Message = "Koniec talii!";
-                return;
-            }
+            if (nextCard == null) { Message = "Koniec talii!"; return; }
 
             if ((guessedHigher && nextCard.Wartosc > _currentCard.Wartosc) ||
                 (!guessedHigher && nextCard.Wartosc < _currentCard.Wartosc))
             {
                 Score++;
-                Message = $"Dobrze! To by³a {nextCard}.";
+                Message = $"Dobrze, To byla {nextCard.Figura} {nextCard.Kolor}.";
+                ShowSuitGuess = false;
             }
             else if (nextCard.Wartosc == _currentCard.Wartosc)
             {
-                Message = $"Remis! To by³a {nextCard}. Punktow bez zmian.";
+                Message = $"Remis, To byla {nextCard.Figura}.";
             }
             else
             {
-                Message = $"Pudlo! To by³a {nextCard}. Twój wynik to {Score}.";
+                Message = $"Pudlo, To byla {nextCard.Figura} {nextCard.Kolor}. Jaki kolor miala nietrafiona karta?";
+                ShowSuitGuess = true;
+
                 GameHistoryService.AddEntry(new GameHistoryEntry
                 {
                     GameName = "Higher or Lower",
-                    Players = "Gracz 1",
+                    Players = _playerService.Players.Count > 0 ? _playerService.Players[0].Login : "Gracz",
                     Winner = $"Wynik: {Score}",
                     Date = DateTime.Now
                 });
+            }
+            CurrentCard = nextCard;
+        }
+
+        [RelayCommand]
+        public void GuessSuit(string suit)
+        {
+            if (CurrentCard.Kolor.ToLower().Contains(suit.ToLower()))
+            {
+                Message = $"Trafiles kolor ({suit})! Bonus +2 punkty";
+                Score += 2;
+            }
+            else
+            {
+                Message = $"Niestety, to nie by³ {suit}. Zaczynamy od zera.";
                 Score = 0;
             }
-
-            CurrentCard = nextCard;
+            ShowSuitGuess = false;
         }
 
         [RelayCommand]
         private void PowrotDoMenu()
         {
-            if (_mainWindowViewModel != null)
-            {
-                _mainWindowViewModel.ShowMainMenu();
-            }
+            _mainWindowViewModel?.ShowMainMenu();
         }
     }
 }
